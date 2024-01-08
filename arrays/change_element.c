04f1389670283101954cefb2ed4894a90092d96e
itfibonacci <levon.tumanyan@outlook.com>
2024-01-08 17:36:12 -0500

    change elements of an array

--- arrays/change_element.c
+++ arrays/change_element.c
@@ -9,10 +9,7 @@ int change_index ( int arr[], int len, int index, int value );
 int main() {
 	int my_nums[] = {4, 5, 5, 2, 123, 222};
 	int len_my_nums = sizeof(my_nums) / sizeof(int);
-	//for (i = 0; i )
 	change_index( my_nums, len_my_nums, 4, 12 );
-	//change_index( my_nums, len_my_nums, 12, 12 );
-	//change_index( my_nums, len_my_nums, 4, 12 );
 	return 0;
 }
 
