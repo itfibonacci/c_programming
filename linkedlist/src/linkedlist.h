5497cef7b5026c1fa5fd2cc860899c1514084181
itfibonacci <levon.tumanyan@outlook.com>
2024-03-20 12:20:54 -0400

    remove linked list to add soon

--- linkedlist/src/linkedlist.h
+++
@@ -1,8 +0,0 @@
-typedef struct Node Node;
-Node* add_node_tail (int value);
-Node* add_node_head (int value);
-int remove_head();
-int remove_tail();
-int get_size();
-void print_list();
-void free_list();
--- linkedlist/src2/linkedlist.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-typedef struct Node Node;
-Node *add_tail(int value);
-size_t get_size();
-void reverse_list_bad();
-void reverse_list_good();
-void print_list();
-void free_list();
